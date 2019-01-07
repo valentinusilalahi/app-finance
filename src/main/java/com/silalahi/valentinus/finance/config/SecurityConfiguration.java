e4dce45cbac618c51213621cd1d30621fcdffedd
valentinusilalahi <valentinus@doku.com>
2019-01-07 15:03:31 +0700

    edit file

--- src/main/java/com/silalahi/valentinus/finance/config/SecurityConfiguration.java
+++ src/main/java/com/silalahi/valentinus/finance/config/SecurityConfiguration.java
@@ -37,7 +37,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		// TODO Auto-generated method stub
 		super.configure(auth);
 		auth
 			.jdbcAuthentication()
@@ -49,13 +48,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Bean
 	private PasswordEncoder passwordEncoder() {
-		// TODO Auto-generated method stub
 		return new BCryptPasswordEncoder(13);
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		// TODO Auto-generated method stub
 		super.configure(http);
 		http
 			.authorizeRequests()
@@ -68,7 +65,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	public void configure(WebSecurity web) throws Exception {
-		// TODO Auto-generated method stub
 		super.configure(web);
 		web.ignoring()
 				.antMatchers("/api/callback/bni/payment")
